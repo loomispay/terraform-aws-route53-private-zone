9378610fb2ac3190519a7801edf3236acc61ff1f
Jakub Socha <jakub.socha@loomispay.com>
2023-08-07 09:07:40 +0200

    Add support for creating multiple association authorization

--- main.tf
+++ main.tf
@@ -21,3 +21,17 @@ resource "aws_route53_zone" "this" {
     var.tags
   )
 }
+
+resource "aws_route53_vpc_association_authorization" "this" {
+  for_each = try(tolist(lookup(each.value, "vpc", [])), [lookup(each.value, "vpc", {})])
+  vpc_id  = each.value.vpc_id
+  zone_id = aws_route53_zone.this.zone_id
+}
+
+#resource "aws_route53_zone_association" "example" {
+#  //Add
+#  provider = "aws.alternate"
+#
+#  vpc_id  = aws_route53_vpc_association_authorization.this.vpc_id
+#  zone_id = aws_route53_vpc_association_authorization.this.zone_id
+#}
